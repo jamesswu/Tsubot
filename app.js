481fe0e6e5b30f604ef54197a35d193eea91de31
James <wu.james@hotmail.com>
2020-09-02 08:49:30 -0400

    first push

---
+++ app.js
@@ -0,0 +1,18 @@
+const settings = require('./resources/config/settings');
+
+
+const temp = require('minecraft-api');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+client.login(settings.BOT_TOKEN);
